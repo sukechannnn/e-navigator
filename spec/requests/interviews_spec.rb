2f7a63d0feb30f38f54a3358fefb02c701416133
punkshiraishi <pharmacy.sy@gmail.com>
2020-05-01 03:13:26 +0900

    Add POST create spec

--- spec/requests/interviews_spec.rb
+++ spec/requests/interviews_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe InterviewsController, type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:interviewer) { FactoryBot.create(:user, :interviewer) }
   let(:interview) { user.interviews.create(params) }
-  let(:params) { { interviewer: interviewer, schedule: Time.now.tomorrow, schedule_status: 'pending' } }
+  let(:params) { { interviewer_id: interviewer.id, schedule: Time.now.tomorrow } }
 
   describe 'GET #new' do
     subject { get new_user_interview_path(user), params: { user_id: user.id }; response }
@@ -18,6 +18,27 @@ RSpec.describe InterviewsController, type: :request do
 
     context 'as an unauthorized' do
       it { is_expected.to have_http_status '302' }
+    end
+  end
+
+  describe 'POST #create' do
+    subject { post user_interviews_path(user), params: { interview: params }; response }
+
+    context 'as an authenticated user' do
+      before { sign_in user }
+      it { is_expected.to have_http_status '302' }
+
+      it 'creates an interview' do
+        expect { subject }.to change(user.interviews, :count).by(1)
+      end
+    end
+
+    context 'as an unauthorized' do
+      it { is_expected.to have_http_status '302' }
+
+      it 'dose not create an interview' do
+        expect { subject }.to change(user.interviews, :count).by(0)
+      end
     end
   end
 

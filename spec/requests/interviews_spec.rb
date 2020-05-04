3ba0d1b619ad7ef44490dbfa07f910119d4eaa90
punkshiraishi <pharmacy.sy@gmail.com>
2020-05-05 01:21:32 +0900

    test: add request specs to confirm if interview#update works

--- spec/requests/interviews_spec.rb
+++ spec/requests/interviews_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe InterviewsController, type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:interviewer) { FactoryBot.create(:user, :interviewer) }
+  let(:another_interviewer) { FactoryBot.create(:user, :interviewer) }
   let(:interview) { user.interviews.create(params) }
   let(:params) { { interviewer_id: interviewer.id, schedule: Time.now.tomorrow } }
+  let(:another_params) { { interviewer_id: another_interviewer.id, schedule: Time.now.tomorrow } }
 
   describe 'GET #new' do
     subject { get new_user_interview_path(user), params: { user_id: user.id }; response }
@@ -69,15 +71,23 @@ RSpec.describe InterviewsController, type: :request do
   end
 
   describe 'PATCH #update' do
-    subject { patch user_interview_path(user, interview), params: { interview: params }; response }
+    subject { patch user_interview_path(user, interview), params: { interview: another_params }; response }
 
     context 'as an authenticated user' do
       before { sign_in user }
       it { is_expected.to have_http_status '302' }
+
+      it 'updates an interview' do
+        expect { subject }.to change { interview.reload.interviewer.id }.from(interviewer.id).to(another_interviewer.id)
+      end
     end
 
     context 'as an unauthorized' do
       it { is_expected.to have_http_status '302' }
+
+      it 'does not update an interview' do
+        expect { subject }.not_to(change { interview.reload.interviewer.id })
+      end
     end
   end
 
